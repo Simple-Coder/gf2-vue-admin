ffe87ba3961f91481766ad89da975ca520050163
dongie <1412834290@qq.com>
2022-06-08 20:58:18 +0800

    table mock url调整

--- web/src/api/table.js
+++ web/src/api/table.js
@@ -2,7 +2,8 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    url: '/vue-admin-template/table/list',
+    // url: '/vue-admin-template/table/list',
+    url: '/api/v1/system/config/list/',
     method: 'get',
     params
   })
