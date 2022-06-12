7ff10e67978805a77923ee07d7609314cf022e1a
dongie <1412834290@qq.com>
2022-06-12 19:46:59 +0800

    tagsview

--- web/src/settings.js
+++ web/src/settings.js
@@ -1,7 +1,11 @@
 module.exports = {
 
   title: 'Vue Admin Template',
-
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true,
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
